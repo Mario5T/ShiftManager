a65ecd42e7a53fa7281852a6b2a5a74d6814233e
Mario5T <singhtiivu@gmail.com>
2025-04-13 18:14:28 +0530

    3

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,13 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
-  const config = await createExpoWebpackConfigAsync(env, argv);
+  const config = await createExpoWebpackConfigAsync({
+    ...env,
+    platform: 'web',
+    babel: {
+      dangerouslyAddModulePathsToTranspile: ['@expo/vector-icons']
+    }
+  }, argv);
+  
   return config;
 };
